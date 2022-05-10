164e795afa7de34c82a7434fa922fb9be941ef26
Juergen Segura <juergen98s@gmail.com>
2022-05-09 18:12:08 -0600

    Detalles

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
+const { src, dest, watch } = require ("gulp");
+const sass = require ("gulp-sass")(require("sass"));
+
 function css(done) {
 
-    //Identificar
-        
-    //Compilar
-
-    //Almacenrla en el disco duro
-
+    src('src/scss/app.scss')//Identificar
+        .pipe( sass() )//Compilar
+        .pipe( dest("build/css"));//Almacenrla en el disco duro
 
     done();//callback que avisa a gulp cuando llegamos al final
 }
+
+
+function dev(done) {
+    watch("src/scss/app.scss", css)
+}
+
+exports.css = css;
+
+exports.dev = dev;
